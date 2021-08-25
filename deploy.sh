09a8d8287135229def7e7750c0b49e87aae47db4
黃柏諺 <roger@ibears.io>
2021-08-25 18:48:22 +0800

    [feat] replace fake logo

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://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@github.com:poyenhuang/sleep-research.git master:gh-pages
+git push -f https://github.com/poyenhuang/sleep-research.git master:gh-pages
 
 cd -
