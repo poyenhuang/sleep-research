a4859cc04977b4080f005bc160897cf4f78b2bfb
黃柏諺 <roger@ibears.io>
2021-08-27 09:45:26 +0800

    [fix] deploy doc

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,4 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/sleep-research/'
     : '/',
-  css: {
-    loaderOptions: {
-      scss: {
-        additionalData: '@import "~@/assets/scss/_fonts.scss";',
-      },
-    },
-  },
 };
